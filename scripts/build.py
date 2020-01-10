b143b3d3e667cee8fa42e29dc0f4f3575ec2b00a
zhaojyun <57133178+zhaojyun@users.noreply.github.com>
2020-01-10 17:57:35 +0800

    Update build.py

--- scripts/build.py
+++ scripts/build.py
@@ -88,14 +88,14 @@ targets = {
 }
 
 supported_builds = {
-    ##'darwin': [ "amd64" ],
+    'darwin': [ "amd64" ],
     "windows": [ "amd64", "i386" ],
     "linux": [ "amd64", "i386", "armhf", "armel", "arm64", "static_amd64", "s390x", "mipsel", "mips","mips64el"]
     ##"freebsd": [ "amd64", "i386" ]
 }
 
 supported_packages = {
-    ##"darwin": [ "tar" ],
+    "darwin": [ "tar" ],
     "linux": [ "deb", "rpm", "tar" ],
     "windows": [ "zip" ]
     ##"freebsd": [ "tar" ]
